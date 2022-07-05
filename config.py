07d62e68fa2d49af471d35f34fe534c1b09af2fe
jen-cg <jennifer.glover@mac.com>
2022-07-05 14:10:00 -0700

    Fixed a path error

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ Paths to data and default parameters
 
 # -----------------------------------------------------------------------------------------------------------------------
 # Paths to model atmospheres
-spherical_model_atm_path = os.path.join( os.getcwd(), 'Data/grids/standard/sphere/')
+spherical_model_atm_path = os.path.join( os.getcwd(), 'asap_lib/Data/grids/standard/sphere/')
 #'/arc5/home/jglover/GRACES/Code/MARCS/grids/standard/sphere/'  # CHANGED
 
-plane_parallel_model_atm_path = os.path.join( os.getcwd(), 'Data/grids/standard/plane/')
+plane_parallel_model_atm_path = os.path.join( os.getcwd(), 'asap_lib/Data/grids/standard/plane/')
 #'/arc5/home/jglover/GRACES/Code/MARCS/grids/standard/plane/'  # CHANGED
 
 # -----------------------------------------------------------------------------------------------------------------------
